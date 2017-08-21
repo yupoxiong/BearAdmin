75d1e5fc98532a1b67f2eaafc39aa6fef55d4eac
于破熊 <8553151@qq.com>
2017-08-21 18:37:59 +0800

    '测试重写项目'

---
+++ application/backend/controller/Controller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * @author yupoxiong<i@yufuping.com>
+ */
+
+namespace app\backend\controller;
+
+use think\Controller as Think;
+
+class Controller extends Think
+{
+    public function index(){
+        return 1;
+    }
+
+}
