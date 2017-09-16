7df1a7c77a437b1fc919010bf516eda9f079993d
于破熊 <8553151@qq.com>
2017-09-16 18:08:36 +0800

    ''

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'bearadmin_yufup',
     // 用户名
