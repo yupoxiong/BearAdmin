3b6d7dd4839ed7083490af1cda9eb164385dbe09
于破熊 <8553151@qq.com>
2021-05-15 03:04:49 +0800

    修复后来自动生成用户相关代码导致前台不能登录的问题

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -39,5 +39,51 @@ class User extends Model
         return $this->belongsTo(UserLevel::class);
     }
 
+    public static function init()
+    {
+        //添加自动加密密码
+        self::event('before_insert', static function ($data) {
+            $data->password = base64_encode(password_hash($data->password, 1));
+        });
 
+        //修改密码自动加密
+        self::event('before_update', function ($data) {
+            $old = (new static())::get($data->id);
+            if ($data->password !== $old->password) {
+                $data->password = base64_encode(password_hash($data->password, 1));
+            }
+        });
+    }
+
+    /**
+     * 用户登录
+     * @param $param
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function login($param)
+    {
+        $username = $param['username'];
+        $password = $param['password'];
+        $user     = self::get(['username' => $username]);
+        if (!$user) {
+            exception('用户不存在');
+        }
+
+        if (!password_verify($password, base64_decode($user->password))) {
+            exception('密码错误');
+        }
+
+        if ((int)$user->status !== 1) {
+            exception('用户被冻结');
+        }
+        return $user;
+    }
+
+    //加密字符串，用在登录的时候加密处理
+    protected function getSignStrAttr($value, $data)
+    {
+        $ua = request()->header('user-agent');
+        return sha1('user_'.$data['id'] . $data['username'] . $ua);
+    }
 }
