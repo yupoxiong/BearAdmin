8da48697403ca77b1621376eee232afe2803c355
于破熊 <8553151@qq.com>
2022-01-20 15:56:30 +0800

    优化后台配置文件

--- app/admin/config/app.php
+++ app/admin/config/app.php
@@ -4,8 +4,12 @@
  * @author yupoxiong<i@yupoxiong.com>
  */
 
-return [
-    // 自定后台义异常页面的模板文件
-    'exception_tmpl'      => app()->getAppPath() . 'view/error/500.html',
+$config = [
+
     'default_ajax_return' => 'html',
 ];
+if (!env('app_debug')) {
+    // 自定后台义异常页面的模板文件
+    $config['exception_tmpl'] = app()->getAppPath() . 'view/error/500.html';
+}
+return $config;
